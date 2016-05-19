10b9445067e593ca0546cbea7b9648a2b2c11ee0
NurFuerSand <tim.staat@live.de>
2016-05-19 22:21:03 +0200

    Changed Program.cs

--- EmotivEngine/Program.cs
+++ EmotivEngine/Program.cs
@@ -10,56 +10,11 @@ namespace EmotivEngine
 {
     class Program
     {
-
-        enum Command { moveLeft, moveRight, moveElswhere }
-        static EmoEngine engine;
         static void Main(string[] args)
         {
-            engine = EmoEngine.Instance;
-
-            Console.Out.WriteLine(engine.ToString());
-            engine.UserAdded += new EmoEngine.UserAddedEventHandler(engine_UserAdded_Event);
-            engine.EmoEngineConnected += new EmoEngine.EmoEngineConnectedEventHandler(connected);
-            engine.EmoEngineDisconnected += new EmoEngine.EmoEngineDisconnectedEventHandler(discon);
-
-            //engine.EmoStateUpdated += new EmoEngine.EmoStateUpdatedEventHandler(stateChange)
-
-            engine.RemoteConnect("127.0.0.1", 1726);
-
-            //engine.Connect();
-            EmoState ES = new EmoState();
-
-            for (int i = 0; i < 1000; i++)
-            {
-                engine.ProcessEvents();
-                Thread.Sleep(100);
-            }
-        }
-
-        static void connected(object sender, EmoEngineEventArgs e)
-        {
-            Console.WriteLine("connected");
-        }
-
-        static void discon(object sender, EmoEngineEventArgs e)
-        {
-            Console.WriteLine("disc");
-        }
-
-        static void engine_UserAdded_Event(object sender, EmoEngineEventArgs e)
-        {
-            Console.WriteLine("User Added Event has occured");
-
-            Profile p = engine.GetUserProfile((uint)e.userId);
-            Console.Out.WriteLine("UserID = " + e.userId);
-            Console.Out.WriteLine(p.ToString());
-            
-        }
-
-        static void stateChange(object sender, EmoEngineEventArgs e)
-        {
-            
-           // Console.WriteLine(ES.GetWirelessSignalStatus());
+            CentralControlEngine cce = CentralControlEngine.Instance;
+            cce.setController(EmoController.getInstance(cce));
+            cce.start();
         }
     }
 }
