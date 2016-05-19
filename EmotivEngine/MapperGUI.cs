ba722248ce418f9db503251f5e5d2f8946cc4549
JonasKlenk <jonas.klenk@gmail.com>
2016-05-20 01:58:52 +0200

    Übertragen der MapperGUI

---
+++ EmotivEngine/MapperGUI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmotivEngine
+{
+    public partial class MapperGUI : Form
+    {
+        private int selectedCommand;
+        private int selectedAction;
+        private int selectedMapping;
+
+
+        public MapperGUI()
+        {
+            InitializeComponent();
+        }
+        
+        private void listCommandTypes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedCommand = listCommandTypes.SelectedIndex;
+        }
+
+        private void listActionTypes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedAction = listCommandTypes.SelectedIndex;
+        }
+
+        private void listMapping_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedMapping = listMapping.SelectedIndex;
+        }
+
+        private void buttonBind_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonDeleteBind_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- EmotivEngine/Program.cs
+++ EmotivEngine/Program.cs
@@ -5,16 +5,25 @@ using System.Text;
 using System.Threading.Tasks;
 using Emotiv;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace EmotivEngine
 {
     class Program
     {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
         static void Main(string[] args)
         {
             CentralControlEngine cce = CentralControlEngine.Instance;
             cce.setController(EmoController.getInstance(cce));
             cce.start();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MapperGUI());
         }
     }
 }
