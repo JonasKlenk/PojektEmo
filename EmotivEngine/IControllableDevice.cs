c4d39c94c2b5d799cdd9e649fc3091ffbb1a5293
Stefan <mark-stefan.struempfler@de.abb.com>
2016-05-24 14:24:31 +0200

    Removed unused using directives in "TcpDevice" and "IControllableDevice"

--- EmotivEngine/IControllableDevice.cs
+++ EmotivEngine/IControllableDevice.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EmotivEngine
+﻿namespace EmotivEngine
 {
     /// <summary>
     /// Interface for basic commands and util functions any device should support
