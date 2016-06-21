fd2728fdac0347fddc41ce9f784ba077cc97fbab
JonasKlenk <jonas.klenk@gmail.com>
2016-06-21 20:36:55 +0200

    dinge

--- EmotivEngine/MainWindow.cs
+++ EmotivEngine/MainWindow.cs
@@ -110,7 +110,7 @@ namespace EmotivEngine
             //cce.registerControllableDevice(new TcpDevice(cce, "127.0.0.1", 23232));
             //UE Simulation
             cce.registerControllableDevice(new TcpDevice(cce, "127.0.0.1", 8890));
-            cce.registerControllableDevice(new TcpDevice(cce, "7.237.127.223", 8890));
+            //cce.registerControllableDevice(new TcpDevice(cce, "7.237.127.223", 8890));
             cce.registerControllableDevice(new DummyDevice(cce));
 
 
